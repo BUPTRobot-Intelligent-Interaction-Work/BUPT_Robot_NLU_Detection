46e7183a9cf4cad092a7fd575b87b391a2f5ffe0
Harris <975313809@qq.com>
2024-11-23 21:43:31 +0800

    rasa branch first commit on github

--- utils.py
+++
@@ -1,9 +0,0 @@
-
-def detect_language(text):
-    chinese_count = sum(1 for char in text if '\u4e00' <= char <= '\u9fff')
-    english_count = sum(1 for char in text if 'a' <= char.lower() <= 'z')
-
-    if chinese_count >= 1:
-        return "Chinese"
-    elif english_count > chinese_count:
-        return "English"
